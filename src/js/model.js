dd6494faed42c75f662a60a2631cb3dba7c792c4
mahmoud-farag <mahmoud32salamn@gmail.com>
2022-05-19 09:38:39 +0200

    fix fraction error

--- src/js/model.js
+++ src/js/model.js
@@ -103,11 +103,11 @@ const handleLocalStorage = function () {
   localStorage.setItem('bookmarks', JSON.stringify(AppState.bookmarks));
 };
 
-// const getLocalStorage = function () {
-//   const bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-//   if (bookmarks) AppState.bookmarks = bookmarks;
-// };
-// getLocalStorage();
+const getLocalStorage = function () {
+  const localData = JSON.parse(localStorage.getItem('bookmarks'));
+  if (localData) AppState.bookmarks = localData;
+};
+getLocalStorage();
 
 export const uploadRecipe = async function (newRecipe) {
   try {
